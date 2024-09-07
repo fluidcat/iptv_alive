c44bbff8a0894116422d749a275f9f2563007c78
fluidcat <Belin0@163.com>
2024-09-07 21:22:16 +0800

    自动提交: 2024-09-07 21:22:16

--- test.js
+++ test.js
@@ -14,8 +14,8 @@ const process = require("child_process")
 
 var exec = process.exec;
 var enter = os.type() == 'Windows_NT'?'\\n':'\n';
-var channelTxt = './live.txt';
-var channelM3u = './live.m3u';
+var channelTxt = './liveTest.txt';
+var channelM3u = './liveTest.m3u';
 
 Date.prototype.Format = function (fmt) {
   var o = {
@@ -38,12 +38,19 @@ Date.prototype.Format = function (fmt) {
 };
 
 async function fetchResponse(url) {
-	return await fetch(url, {
-      method: "GET",
-      headers: {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0"},
-      responseType: "stream",
-      timeout: 3000,
-    });
+	let fetchStart = new Date().getTime();
+	try {
+		return await fetch(url, {
+		  method: "GET",
+		  headers: {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0"},
+		  responseType: "stream",
+		  timeout: 3000,
+		});
+	} catch (e) {
+		throw e;
+	} finally {
+		console.log(url+', 响应时间：'+ ((new Date().getTime())-fetchStart) +'ms');
+	}
 }
 async function verifyurl(url) {
   try {
@@ -54,9 +61,24 @@ async function verifyurl(url) {
 	if (res.status != 200){
 //		console.log("响应错误:"+res.status+", "+url)
 		return false;
-/*	} else if (url.includes('.m3u8') || res.headers.get('content-type') == 'application/vnd.apple.mpegurl'){
-		let body = await res.text();
-	} else if(res.headers.get('content-type') == 'video/x-flv') {
+	} else if (url.includes('.m3u8') || res.headers.get('content-type') == 'application/vnd.apple.mpegurl'){
+		let tsPrefix = url.replace(url.split('/').pop(),'')
+		let tsTxt = await res.text();
+		// 判断是否m3u文件
+		if(!tsTxt.startsWith('#EXTM3U')) {
+			return false;
+		}
+		let ts_list = tsTxt.split("\n")
+			.filter(e=>e.length>0&&!e.startsWith('#'))
+			.map(e=>e.startsWith('http')?e:tsPrefix+e.trim());
+		let speed = await checkSpeed(ts_list)
+		if(speed == 0) {
+			console.log('无响应：'+url+'\nts:'+ts_list.join('\n')+'\nm3u8:\n'+tsTxt);
+			return false;
+		}
+
+
+/*	} else if(res.headers.get('content-type') == 'video/x-flv') {
 		let speed = await checkSpeed([url])
 */	} else {
 //		console.log("headers:"+JSON.stringify(res.headers))
@@ -71,24 +93,30 @@ async function verifyurl(url) {
 async function checkSpeed(urls) {
 	let chunkSize = 0;
 	let startTime = new Date().getTime();
+	for(var index in urls){
+		// 太多个ts
+		if(index >= 1) {
+			break;
+		}
+		let url = urls[index];
+		
+		let response = await fetchResponse(url);
+		if (response.status != 200){
+			chunkSize=0;
+			break;
+		}
+		chunkSize+=parseInt(response.headers.get('content-length'));
+	}
+	/*
 	let waitHandler = new Promise((resolve) => {
 		setTimeout(async()=>{
-			for(var index in urls){
-				let url = urls[index];
-				console.log("开始："+new Date().getTime())
-				let response = await fetchResponse(url);
-				if (response.status != 200){
-					chunkSize=0;
-					break;
-				}
-				chunkSize+=response.body.readableLength;
-				console.log("结束："+new Date().getTime())
-			}
+
 			resolve();
 		}, 12000);
 	});
 
 	await waitHandler;
+	*/
 	let costSecond = ((new Date().getTime()) - startTime)/1000;
 	if(chunkSize <= 0) {
 		return 0;
@@ -206,12 +234,14 @@ async function checkallurl(data) {
     var isesplit = ise.trim().split(",");
     if (isesplit.length >= 2) {
       if (isesplit[1].trim().indexOf("http") == 0) {
+		let stime = new Date().getTime()
         if (await verifyurl(isesplit[1].trim())) {
           console.log("添加:", ise);
           ret += "\n" + ise;
         } else {
           console.log("排除:", ise);
         }
+		console.log('verifyurl时间：'+((new Date().getTime())-stime)+'ms\n\n');
         return;
       }
     }
